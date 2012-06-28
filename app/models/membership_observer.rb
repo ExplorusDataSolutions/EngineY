d608b0cc4c597ecde4219e235d02d8617839469c
AdamDotCom <adam@kahtava.com>
2012-06-28 13:55:55 -0600

    Community updates, fixed an issue where group creators were being emailed upon creating a new group

--- app/models/membership_observer.rb
+++ app/models/membership_observer.rb
@@ -1,10 +1,12 @@
 class MembershipObserver < ActiveRecord::Observer
 
   def after_create(membership)
-    if (membership.group.private)
-      MembershipMailer.deliver_private_member_request(membership)
-    else
-      MembershipMailer.deliver_member_joined(membership)
+    if !(membership.user_id == membership.group.creator.id)
+      if membership.group.private
+        MembershipMailer.deliver_private_member_request(membership)
+      else
+        MembershipMailer.deliver_member_joined(membership)
+      end
     end
   end
   
